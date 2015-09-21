7dc99b882cd81b07f4fc10e4226a7f4d7609f97b
Jeffrey Cheng <JeffreyCheng92@gmail.com>
2015-09-21 14:34:37 -0700

    added comments for description

--- Calculator/calculate.rb
+++ Calculator/calculate.rb
@@ -1,13 +1,23 @@
 def calculate(input)
+  # Builds an array of numbers and operations
   input_array = input.split("")
+
+  # Stack of the indices of parentheses to evaluate them first
   priority_expressions_idx = []
+
+  # Used to record indices of the close parenthesis
   idx = 0
 
+  # O(n)
+  # Records the locations of the parenthesis
   input_array.each_with_index do |el, i|
     if el == "("
       priority_expressions_idx << [i, nil]
     elsif el == ")"
       length = priority_expressions_idx.length - 1
+
+      # Checks the last parenthesis recorded and see if the closed parenthesis
+      # was recorded.
       if length - idx >= 0 && priority_expressions_idx[length - idx].last.nil?
         priority_expressions_idx[length - idx][-1] = i
         idx += 1
@@ -17,22 +27,35 @@ def calculate(input)
     end
   end
 
+  # Accounts for the change in the array due to deletions
   change = 0
+
+  # O(n)
   priority_expressions_idx.reverse.each do |exp|
     expression = input_array[exp[0] + 1... exp[1] - change]
+
+    # O(number of elements in expression)
     answer = evaluate(expression)
+
+    # Puts evaluated parenthesis expression into array
     input_array[exp[0]] = answer
 
+    # Deletes evaluated parenthesis expression accounting for previous deletes
     (exp[1] - change).downto(exp[0] + 1) do |i|
       input_array.delete_at(i)
       change += 1
     end
   end
 
+  # Evaluates rest of expression after all parentheses are evaluated
   evaluate(input_array)
 end
 
+# Evaluates expressions
+# O(n)
 def evaluate(input)
+
+  # Evaluates multiplication and division first
   while input.include?("*") || input.include?("/")
     input.each_with_index do |el, i|
       if el == "*"
@@ -49,6 +72,8 @@ def evaluate(input)
     end
   end
 
+  # Evaluates addition and subtraction after all multiplication and division
+  # are completed.
   while input.include?("+") || input.include?("-")
     input.each_with_index do |el, i|
       if el == "+"
@@ -67,6 +92,7 @@ def evaluate(input)
   input.first.to_s
 end
 
+# Math helper functions
 def plus(num1, num2)
   num1.to_i + num2.to_i
 end
